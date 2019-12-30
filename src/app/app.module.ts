786a6b690f45914c5655dd41fc61a1186791f70c
nkumar003c <Nitin_kumar@cable.comcast.com>
2019-12-30 18:06:44 -0500

    Refactor the routing of header module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { routing } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -11,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
